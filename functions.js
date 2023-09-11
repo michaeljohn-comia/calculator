ad5a933225c659935c100434ef42e83ff06ef4a3
yuiii <126733367+michaeljohn-comia@users.noreply.github.com>
2023-09-11 17:09:23 +0800

    added js functions
    fixed some style, added padding in textscreen of the calculator;
    added some functions

--- functions.js
+++ functions.js
@@ -1,1 +1,35 @@
-var itemsOnScreen = [];
+var itemsOnScreen = "";
+var result = "";
+
+var screenItems = document.querySelector("#screenItems");
+var calcBtn = document.querySelectorAll(".btnNum");
+
+function resultOfEquation(itemsOnScreen){
+    try {
+        result = eval(itemsOnScreen);
+        if (isNaN(result)) {
+            throw new Error("Invalid input");
+        }
+        screenItems.textContent = result;
+    } catch (error) {
+        screenItems.textContent = "Error";
+    }
+    itemsOnScreen = "";
+    // result = eval(itemsOnScreen);
+    // screenItems.textContent = result;
+    // itemsOnScreen = "";
+}
+
+calcBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        var btnValue = button.textContent;
+        itemsOnScreen += btnValue;
+        screenItems.textContent = itemsOnScreen;
+    })
+})
+
+var btnEqual = document.querySelector("#btnEqual");
+btnEqual.addEventListener("click", function(){
+    resultOfEquation(itemsOnScreen);
+    itemsOnScreen = ""; 
+});
